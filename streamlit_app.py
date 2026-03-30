c042ec5ddca325d8b212598c5e36cc4d52f0eee9
Mona kashyap <monakashyap0300@gmail.com>
2026-03-30 20:33:23 +0530

    Fix string quotes in CSV URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title('Machine Learning App')
-df=pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
+df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
 
                
 
