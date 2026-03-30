0c020306d86ec13679290dd636ed5455b2ee0776
Mona kashyap <monakashyap0300@gmail.com>
2026-03-30 20:36:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 st.title('Machine Learning App')
 st.write('This app builds a machine learning model')
+with st.expander('Data'):
+  st.write('**Raw data**')
 
 df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
 df
