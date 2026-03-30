b6953038fee9cd0ce30ed9eafc911239917dd605
Mona kashyap <monakashyap0300@gmail.com>
2026-03-30 20:35:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 
 st.title('Machine Learning App')
-df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+st.write('This app builds a machine learning model')
 
+df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+df
                
 
-st.write('This app builds a machine learning model')
+
