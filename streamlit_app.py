39fdbe2eb17a4cd217204d3b78a15faabcb382f3
Mona kashyap <monakashyap0300@gmail.com>
2026-04-01 19:40:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.ensemble imporrt RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 st.title('Machine Learning App')
 st.write('This app builds a machine learning model')
