2ed73124a1c94678fa4e474d24407d7ae6a23bd9
Mona kashyap <monakashyap0300@gmail.com>
2026-04-01 17:27:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,28 @@ y
 
 with st.expander('Data visualization'):
   st.scatter_chart(data= df, x='bill_length_mm', y= 'body_mass_g', color= 'species')
+
+with st.sidebar:
+  st.header('Input Features')
+  # "", "", "", ""
+island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+gender=  st.selectbox('Gender', ('male', 'female'))
+bill_length_mm= st.slider('Bill length (mm)', 32.1, 59.6, 43..9)
+bill_depth_mm= st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+flipper_length_mm= st.slider('Flipper length (mm)', 172.8, 231.0, 201.0)
+body_mass_ st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
